1241a7d0317d744d86ecaf56d07cf43b86a72156
RekhaDeenan <82083743+RekhaDeenan@users.noreply.github.com>
2021-04-13 15:47:57 +0530

    Assignment2(Implement the common DataProvider)

--- BaseClass.java
+++ BaseClass.java
@@ -34,3 +34,63 @@ public class BaseClass {
 driver.close();
 	}
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+	package week5.day2;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass{
+
+	@Test(dataProvider="fetchData")
+	public void runCreateLead(String compname,String fname,String lname)  {
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(compname);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(fname);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lname);
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+	@DataProvider(name="fetchData")
+	public String[][] sendData() {
+		String[][] data=new String[3][3];
+		data[0][0]="Test Leaf";
+		data[0][1]="Rekha";
+		data[0][2]="D";
+		
+		data[1][0]="Test Leaf";
+		data[1][1]="Venba";
+		data[1][2]="T";
+		
+		data[2][0]="TL";
+		data[2][1]="Mani";
+		data[2][2]="J";
+		return data;
+	}
+}
+
+----------------------------------------------------------------------------------------------------------------------------------
+	<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+<suite name="assignmentdataprovider">
+<parameter name="url" value="http://leaftaps.com/opentaps"></parameter>
+<parameter name="username" value="DemoSalesManager"></parameter>
+<parameter name="password" value="crmsfa"></parameter>
+
+  <test thread-count="5" name="Test">
+    <classes>
+      <class name="week5.day2.CreateLead"/>
+    </classes>
+  </test> <!-- Test -->
+</suite> <!-- Suite -->
+
+			
